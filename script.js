7d6437a9f77f74df651789b624c6c4eb5bdf338e
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-10 22:22:53 +0700

    make navbar more cool

---
+++ script.js
@@ -0,0 +1,64 @@
+// const arrow = document.querySelector(".arrow-up");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 0) {
+//     arrow.style.display = "grid";
+//     setTimeout(() => {
+//       arrow.style.opacity = 1;
+//     }, 0);
+//   } else {
+//     arrow.style.opacity = 0;
+//     setTimeout(() => {
+//       arrow.style.display = "none";
+//     }, 300);
+//   }
+// });
+
+const contactForm = document.querySelector(".contact-form");
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const sender = contactForm.querySelector("#sender").value.trim();
+  const email = contactForm.querySelector("#email").value.trim();
+  const message = contactForm.querySelector("#message").value.trim();
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+  const finalMessage = `${sender} (${email}):\n${message.replace(/\n/g, "\n")}`;
+  const encodedMessage = encodeURIComponent(finalMessage);
+  const whatsappUrl = `https://wa.me/6283802598364?text=${encodedMessage}`;
+  document.location.href = whatsappUrl;
+});
+
+function isTopOfElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return rect.top >= 0 && rect.top <= 0.5 * window.innerHeight;
+}
+
+let sectionLocation = "home";
+const menuItems = {
+  home: "abbr[title='Home'] a",
+  about: "abbr[title='About'] a",
+  skills: "abbr[title='Skills'] a",
+  projects: "abbr[title='Projects'] a",
+  certifications: "abbr[title='Certifications'] a",
+  contact: "abbr[title='Contact'] a",
+};
+
+window.addEventListener("scroll", function () {
+  const sections = document.querySelectorAll("main .container");
+  sections.forEach((section) => {
+    if (isTopOfElementInViewport(section)) {
+      sectionLocation = section.id.trim();
+      for (const item in menuItems) {
+        const menuItem = document.querySelector(menuItems[item]);
+        if (item === sectionLocation) {
+          menuItem.classList.add("active");
+        } else {
+          menuItem.classList.remove("active");
+        }
+      }
+    }
+  });
+});
