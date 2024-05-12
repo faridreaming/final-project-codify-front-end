67f16f9c4709a6329705518a4246293874eba527
faridrawing <muhammad.farid.yamin@gmail.com>
2024-05-12 21:15:20 +0700

    add dark mode toggle

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ contactForm.addEventListener("submit", (event) => {
   const whatsappUrl = `https://wa.me/6283802598364?text=${encodedMessage}`;
   document.location.href = whatsappUrl;
 });
-
 function isTopOfElementInViewport(el) {
   const rect = el.getBoundingClientRect();
   return rect.top >= 0 && rect.top <= 0.5 * window.innerHeight;
@@ -31,7 +30,16 @@ const menuItems = {
 const navbar = document.querySelector("header nav");
 let deviceWidth = window.innerWidth;
 let navbarHeight = 0;
-
+if (deviceWidth < 1020) {
+  navbar.style.bottom = "-66px";
+  navbarHeight = 66;
+} else if (deviceWidth < 561) {
+  navbar.style.bottom = "-58px";
+  navbarHeight = 58;
+} else {
+  navbar.style.bottom = "0px";
+  navbarHeight = 0;
+}
 window.addEventListener("resize", function () {
   deviceWidth = window.innerWidth;
   if (deviceWidth < 1020) {
@@ -42,11 +50,15 @@ window.addEventListener("resize", function () {
     navbarHeight = 0;
   }
 });
-
 window.addEventListener("scroll", function () {
-  if (window.scrollY > navbarHeight) navbar.style.bottom = "0";
-  else navbar.style.bottom = `-${navbarHeight}px`;
   const sections = document.querySelectorAll("main .container");
+  let isAtHome = window.scrollY < navbarHeight / 2;
+
+  if (isAtHome) {
+    navbar.style.bottom = `-${navbarHeight + 1}px`;
+  } else {
+    navbar.style.bottom = "0";
+  }
   sections.forEach((section) => {
     if (isTopOfElementInViewport(section)) {
       sectionLocation = section.id.trim();
@@ -60,4 +72,21 @@ window.addEventListener("scroll", function () {
       }
     }
   });
+});
+const toggleDarkMode = document.getElementById("toggle-dark-mode");
+function setDarkMode(isDarkMode) {
+  localStorage.setItem("dark-mode", isDarkMode);
+  if (isDarkMode) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+}
+toggleDarkMode.addEventListener("click", () => {
+  const isDarkMode = localStorage.getItem("dark-mode") === "true";
+  setDarkMode(!isDarkMode);
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const isDarkMode = localStorage.getItem("dark-mode") === "true";
+  setDarkMode(isDarkMode);
 });
